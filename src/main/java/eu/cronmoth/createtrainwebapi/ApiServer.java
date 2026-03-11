886d975a742e70d4be81ff1016fe452f6af15ba3
Nils Andreas Svee <me@lochnair.net>
2026-03-11 23:53:51 +0700

    fix: add import for RejectedExecutionException

--- src/main/java/eu/cronmoth/createtrainwebapi/ApiServer.java
+++ src/main/java/eu/cronmoth/createtrainwebapi/ApiServer.java
@@ -17,6 +17,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.RejectedExecutionException;
 
 public class ApiServer {
     private Undertow server;
