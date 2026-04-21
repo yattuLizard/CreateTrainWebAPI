b07fdbad91b4059e4a3238b3c15bfc226e46e26f
User <henke.benedikt@gmail.com>
2026-04-22 00:06:14 +0200

    no yarn mappings

--- src/main/java/eu/cronmoth/createtrainwebapi/mixin/MinecraftMixin.java
+++ src/main/java/eu/cronmoth/createtrainwebapi/mixin/MinecraftMixin.java
@@ -1,19 +1,19 @@
 package eu.cronmoth.createtrainwebapi.mixin;
 
 import eu.cronmoth.createtrainwebapi.CreateTrainWebAPIMod;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.RunArgs;
+import net.minecraft.client.Minecraft;
+
+import net.minecraft.client.main.GameConfig;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
+@Mixin(Minecraft.class)
 public class MinecraftMixin {
-
     @Inject(method = "<init>", at = @At("TAIL"))
-    private void onInit(RunArgs args, CallbackInfo ci) {
+    private void example$init(GameConfig gameConfig, CallbackInfo ci) {
         CreateTrainWebAPIMod.LOGGER.info("Hello from {}", CreateTrainWebAPIMod.NAME);
     }
 }
