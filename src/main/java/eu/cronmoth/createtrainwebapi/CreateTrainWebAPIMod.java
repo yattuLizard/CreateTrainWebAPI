0ff7d5b54b017e779e322d65fc097aee00e9a5f1
User <henke.benedikt@gmail.com>
2026-04-27 20:07:55 +0200

    actually load config

--- src/main/java/eu/cronmoth/createtrainwebapi/CreateTrainWebAPIMod.java
+++ src/main/java/eu/cronmoth/createtrainwebapi/CreateTrainWebAPIMod.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
+import java.nio.file.Paths;
+
 public class CreateTrainWebAPIMod implements ModInitializer {
 
     public static final String ID = "createtrainwebapi";
@@ -21,6 +23,7 @@ public class CreateTrainWebAPIMod implements ModInitializer {
         // Register server start/stop handlers
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
             try {
+                Config.load(Paths.get("./config"));
                 String host = Config.SERVER_HOST;
                 int port = Config.SERVER_PORT;
                 String trainModelPath = Config.TRAIN_MODEL_PATH;
